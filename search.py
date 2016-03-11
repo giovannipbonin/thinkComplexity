9a19c5a7401454544976587f99ca2d936be53b14
Giovanni Pascotto Bonin <pbonin.giovanni@gmail.com>
2016-03-10 21:11:55 -0500

    implement floyd-marshall algo

--- search.py
+++ search.py
@@ -17,3 +17,25 @@ def dijkstraSearch(graph, start):
                 distances[v] = d + 1
                 queue.put(v)
     return distances
+
+def floydMarshall(graph):
+    dist = [[float('inf') for i in range(verticesLen)] for i in range(verticesLen)] 
+    vertices = {}
+    verticesLen = 0
+    verticesList = graph.vertices()
+    for v in verticesList:
+       vertices[v] = count
+       dist[count][count] = 0
+       verticesLen += 1
+    edges = graph.edges()
+    for e in edges:
+        v1, v2 = e
+        i, j = vertices[v1], vertices[v2]
+        dist[i][j] = 1
+    for k in range(verticesLen):
+        for i in range(verticesLen):
+            for j in range(verticesLen):
+                if (dist[i][j] > dist[i][k] + dist[k][j]):
+                    dist[i][j] = dist[i][k] + dist[k][j]
+    return dist
+          
