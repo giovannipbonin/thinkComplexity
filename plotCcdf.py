521fdac991845b099c4c6e55149e8ea9634cc865
Giovanni Pascotto Bonin <pbonin.giovanni@gmail.com>
2016-03-13 16:40:19 -0400

    modify plot_cdf function to take one less argument

--- plotCcdf.py
+++ plotCcdf.py
@@ -1,24 +1,26 @@
 import matplotlib.pylab as pylab
 import random
 import Cdf
+import sys
 
 
-def plot_ccdf(values, prob, xscale, yscale):
+def plot_ccdf(values, xscale, yscale):
     pylab.yscale(yscale)
+    cdf = Cdf.MakeCdfFromList(values)
+    values, prob = cdf.Render()
     pylab.xscale(xscale)
     compProb = [1 - e for e in prob] 
     pylab.plot(values, compProb)
     
 
-values = [random.expovariate(10) for i in range(1000)]
-cdfExpo = Cdf.MakeCdfFromList(values)
-y1, p1 = cdfExpo.Render()
-values = [random.paretovariate(1) for i in range(1000)]
-cdfPareto = Cdf.MakeCdfFromList(values)
-y2, p2 = cdfPareto.Render()
+def main(script, *args):
+    values = [random.expovariate(10) for i in range(1000)]
+    pylab.subplot(3, 1, 1)
+    plot_ccdf(values, 'linear', 'log')
+    pylab.subplot(3, 1, 2)
+    values = [random.paretovariate(1) for i in range(1000)]
+    plot_ccdf(values, 'log', 'log')
+    pylab.show()
 
-pylab.subplot(2, 1, 1)
-plot_ccdf(y1, p1, 'linear', 'log')
-pylab.subplot(2, 1, 2)
-plot_ccdf(y2, p2, 'log', 'log')
-pylab.show()
+if __name__ == '__main__':
+    main(*sys.argv)
