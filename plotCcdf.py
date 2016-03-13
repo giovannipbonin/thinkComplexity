102f23b011550009f6f5bbb7db265ec37449a6b7
Giovanni Pascotto Bonin <pbonin.giovanni@gmail.com>
2016-03-13 19:48:41 -0400

    add plot_cdf method

--- plotCcdf.py
+++ plotCcdf.py
@@ -11,13 +11,20 @@ def plot_ccdf(values, xscale, yscale):
     pylab.xscale(xscale)
     compProb = [1 - e for e in prob] 
     pylab.plot(values, compProb)
+
+def plot_cdf(values, xscale, yscale):
+    pylab.yscale(yscale)
+    cdf = Cdf.MakeCdfFromList(values)
+    values, prob = cdf.Render()
+    pylab.xscale(xscale)
+    pylab.plot(values, prob)
     
 
 def main(script, *args):
     values = [random.expovariate(10) for i in range(1000)]
-    pylab.subplot(3, 1, 1)
+    pylab.subplot(2, 1, 1)
     plot_ccdf(values, 'linear', 'log')
-    pylab.subplot(3, 1, 2)
+    pylab.subplot(2, 1, 2)
     values = [random.paretovariate(1) for i in range(1000)]
     plot_ccdf(values, 'log', 'log')
     pylab.show()
