7f0b6d67f9ce4e1c31b1a05a230914f71c1be1bf
Giovanni Pascotto Bonin <pbonin.giovanni@gmail.com>
2016-05-03 16:36:45 -0400

    implement pseudorandom program

--- randomGenerator.py
+++ randomGenerator.py
@@ -1,3 +1,4 @@
+import random
 def linear_congruential_generator(seed):
     n = seed
     a = 22695477
@@ -7,4 +8,13 @@ def linear_congruential_generator(seed):
         n = (a*n + c) % m
         yield n
    
+
+i = 0
+#gen = linear_congruential_generator(5)
+while (i < 10**5):
+#    print(next(gen))
+    print(random.randint(0, 2**32))
+    i += 1
+
+
 
